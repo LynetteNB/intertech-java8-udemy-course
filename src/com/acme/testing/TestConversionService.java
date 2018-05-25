11679ff811353ddff7d0e375b6b0dae959affe99
Lynette <lyna8907@gmail.com>
2018-05-25 13:04:21 -0500

    completed lab 5

--- src/com/acme/testing/TestConversionService.java
+++ src/com/acme/testing/TestConversionService.java
@@ -1,30 +1,33 @@
-//package com.acme.testing;
-//
-//import com.acme.utils.ConversionService;
-//
-//public class TestConversionService {
-//
-//	/**
-//	 * @param args
-//	 */
-//	public static void main(String[] args) {
-//		System.out.println(ConversionService.fluidOunces(1.1f));
-//		System.out.println(ConversionService.gallons(2.2f));
-//		System.out.println(ConversionService.grams(30));
-//		System.out.println(ConversionService.milliliters(40));
-//		System.out.println(ConversionService.ounces(50));
-//		System.out.println(ConversionService.pints(6.6f));
-//		System.out.println(ConversionService.pounds(7.7));
+package com.acme.testing;
+
+import com.acme.utils.ConversionService;
+
+import java.math.BigDecimal;
+
+public class TestConversionService {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(ConversionService.fluidOunces(1.1f));
+		System.out.println(ConversionService.gallons(2.2f));
+		System.out.println(ConversionService.grams(new BigDecimal(30)));
+		System.out.println(ConversionService.milliliters(40));
+		System.out.println(ConversionService.ounces(new BigDecimal(50)));
+		System.out.println(ConversionService.pints(6.6f));
+		System.out.println(ConversionService.pounds(new BigDecimal(7.7)));
 
 		// can you figure out why these do not compile
-		//ConversionService.fluidOunces(1.1);
-		//ConversionService.grams(30L);
-		//ConversionService.milliliters(4.0);
-		//short grams = ConversionService.grams(30);
-		//byte byteGrams = ConversionService.grams(30);
+		ConversionService.fluidOunces(1.1F);
+		ConversionService.grams(new BigDecimal(30));
+		ConversionService.milliliters(4);
+		BigDecimal grams = ConversionService.grams(new BigDecimal(30));
+		BigDecimal byteGrams = ConversionService.grams(new BigDecimal(30));
+        System.out.println(grams);
+        System.out.println(byteGrams);
 
 		// why do these still work even though the types are different
-		/*
 		System.out.println("------------------------------------");
 		double ounces = ConversionService.fluidOunces(1.1f);
 		System.out.println(ounces);
@@ -32,34 +35,31 @@
 		System.out.println(milliliters);
 		double decimalmilliliters = ConversionService.milliliters(40);
 		System.out.println(decimalmilliliters);
-		short s = 30;
+		BigDecimal s = new BigDecimal(30);
 		System.out.println(ConversionService.grams(s));
 		byte b = 4;
 		System.out.println(ConversionService.milliliters(b));
 		char z = 'z';
 		System.out.println(ConversionService.milliliters(z));
 		System.out.println(ConversionService.gallons(200));
-		System.out.println(ConversionService.ounces(50.5f));
+		System.out.println(ConversionService.ounces(new BigDecimal(50.5f)));
 		System.out.println(ConversionService.pints(6L));
-		System.out.println(ConversionService.pounds(7L));
-		*/
+		System.out.println(ConversionService.pounds(new BigDecimal(7L)));
 
 		// compare these results.  Can you tell why they are different?
-		/*
+
 		System.out.println("------------------------------------");
 		float bigGallons = ConversionService.gallons(123456789123456789L);
 		System.out.println(bigGallons);
 		double bigGallons2 = 123456789123456789L * 0.2642;
 		System.out.println(bigGallons2);
-		*/
 
-		/*
 		System.out.println("------------------------------------");
-		int bigGrams = ConversionService.grams(1234567890);
+		BigDecimal bigGrams = ConversionService.grams(new BigDecimal(1234567890));
 		System.out.println(bigGrams);
 		long bigGrams2 = 1234567890L * 1000L;
 		System.out.println(bigGrams2);
-		*/
-//	}
-//
-//}
+
+	}
+
+}
--- src/com/acme/utils/ConversionService.java
+++ src/com/acme/utils/ConversionService.java
@@ -1,4 +1,48 @@
 package com.acme.utils;
 
+import java.math.BigDecimal;
+
 public class ConversionService {
+    /**
+     * weight conversion rates
+     */
+    public static BigDecimal kilogramToPounds = new BigDecimal(2.2046);
+    public static BigDecimal kilogramToGrams = new BigDecimal(1000);
+    public static BigDecimal kilogramToOunces = new BigDecimal(35.274);
+
+    /**
+     * volume conversion rates
+     */
+    public static float literToFluidOunce = 33.814f;
+    public static float literToGallon = 0.2642f;
+    public static float literToPints = 2.1134f;
+    public static short literToMilliliters = 1_000;
+
+    public static BigDecimal pounds (BigDecimal kilograms) {
+        return kilograms.multiply(kilogramToPounds);
+    }
+
+    public static BigDecimal grams (BigDecimal kilograms) {
+        return kilograms.multiply(kilogramToGrams);
+    }
+
+    public static BigDecimal ounces (BigDecimal kilograms) {
+        return kilograms.multiply(kilogramToOunces);
+    }
+
+    public static float gallons (float liters) {
+        return liters * literToGallon;
+    }
+
+    public static float fluidOunces (float liters) {
+        return liters * literToFluidOunce;
+    }
+
+    public static float pints (float liters) {
+        return liters * literToPints;
+    }
+
+    public static int milliliters (int liters) {
+        return liters * literToMilliliters;
+    }
 }
