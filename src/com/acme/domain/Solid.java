64a7f9bab6b5ae0bc268bd8cffa5deefed17375e
Lynette <lyna8907@gmail.com>
2018-05-26 14:12:43 -0500

    completed lab 9 inheritance

---
+++ src/com/acme/domain/Solid.java
@@ -0,0 +1,36 @@
+package com.acme.domain;
+
+public class Solid extends Good{
+    private double width;
+    private double length;
+
+    public Solid(String name, int modelNumber, double height, UnitOfMeasureType unitOfMeasure, boolean flammable, double weightPerUofM, double width, double length) {
+        super(name, modelNumber, height, unitOfMeasure, flammable, weightPerUofM);
+        this.width = width;
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double volume() {
+        return length * width * getHeight();
+    }
+
+    public String toString() {
+        return super.toString() + " that is " + volume() + " " + getUnitOfMeasure() + " in size";
+    }
+}
