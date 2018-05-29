d68ddff96e57d490529d82715e894e2f3a8e7dc7
Lynette <lyna8907@gmail.com>
2018-05-28 19:17:34 -0500

    completed lab 13 w/ local date time

--- src/com/acme/testing/TestOrders.java
+++ src/com/acme/testing/TestOrders.java
@@ -6,13 +6,16 @@ import com.acme.domain.Service;
 import com.acme.domain.Solid;
 import com.acme.utils.MyDate;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 public class TestOrders {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Order.setRushable((orderDate, orderAmount) -> orderAmount > 1500);
+		Order.setRushable((orderDate, orderAmount) -> Period.between(LocalDate.of(orderDate.getYear(), orderDate.getMonth(), orderDate.getDay()), LocalDate.now()).getMonths() >= 1);
 
 		MyDate date1 = new MyDate(1,20,2008);
 		Solid s1 = new Solid("Acme Anvil", 1668, 0.3, Good.UnitOfMeasureType.CUBIC_METER, false, 500, 0.25, 0.3);
@@ -21,6 +24,10 @@ public class TestOrders {
 		MyDate date2 = new MyDate(4,10,2008);
 		Solid s2 = new Solid("Acme Balloon", 1401, 15, Good.UnitOfMeasureType.CUBIC_FEET, false, 10, 5, 5);
 		Order balloons = new Order(date2, 1000.00, "Bugs Bunny", s2, 125);
+
+		MyDate hammerDate = new MyDate(5, 17, 2018);
+		Solid hammerType = new Solid("Acme Hammer", 281, 0.3, Good.UnitOfMeasureType.CUBIC_METER, false, 100, 0.25, 0.3);
+		Order hammer = new Order(hammerDate, 10.00, "Wile E Coyote", hammerType, 10);
 
 		System.out.println(anvil);
 		System.out.println(balloons);
@@ -38,6 +45,7 @@ public class TestOrders {
 
 		System.out.println("Anvil isPriorityOrder: " + anvil.isPriorityOrder());
 		System.out.println("Balloons isPriorityOrder: " + balloons.isPriorityOrder());
+		System.out.println("Hammer isPriorityOrder: " + hammer.isPriorityOrder());
 
 		MyDate date3 = new MyDate(5, 20, 2008);
 		Order anotherAnvil = new Order(date3, 200, "Road Runner");
